79724cd7923a34f0bf9bc8e0aa84c781d07c6015
Tulsi Prasad <tulsi.prasad50@gmail.com>
2020-05-30 09:33:19 +0530

    :alien: remove old api code

--- src/containers/FoodItems/FoodItems.js
+++ src/containers/FoodItems/FoodItems.js
@@ -7,7 +7,9 @@ import classes from "./FoodItems.module.css";
 class FoodItems extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            isLoading: true,
+        };
     }
 
     static getDerivedStateFromProps(props, state) {
@@ -24,68 +26,11 @@ class FoodItems extends Component {
                 fat,
                 calorie,
             },
-            foodItems: [],
         };
     }
 
-    // componentDidUpdate() {
-    //     async function callAPI() {
-    //         // Fetch data from API and load it in state
-    //         const apiKey = "6b42ea9d2aa945d4890323978a300b93";
-    //         const o = {
-    //             typed: "Pizza",
-    //             nutrients: {
-    //                 carb: 20,
-    //                 protein: 40,
-    //                 fat: 60,
-    //                 calorie: 80,
-    //             },
-    //         };
-
-    //         let foods = await (
-    //             await api.get(
-    //                 `/complexSearch?apiKey=${apiKey}&query=${o.typed}&maxCarbs=${o.nutrients.carb}&maxFats=${o.nutrients.fat}`
-    //             )
-    //         ).data;
-
-    //         console.log(foods);
-
-    //         return foods.results;
-    //     }
-
-    //     const foodData = callAPI();
-    //     this.setState({ foodItems: foodData });
-    // }
-
     render() {
-        let items = null;
-        let results = this.state.foodItems;
-
-        if (results.length > 0) {
-            items = results.map((item) => {
-                let nuts = null;
-
-                if (item.nutrients.length > 0) {
-                    nuts = item.nutrients.map((i) => {
-                        return {
-                            name: i.name,
-                            amount: i.amount + i.unit,
-                        };
-                    });
-                }
-
-                return (
-                    <FoodItem
-                        id={item.id}
-                        title={item.title}
-                        image={item.image}
-                        nutriVal={nuts}
-                    />
-                );
-            });
-        }
-
-        return <div className={classes.FoodItems}>{items}</div>;
+        return <h1>Loaded</h1>;
     }
 }
 
