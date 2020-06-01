351d0ba2be0e200e7d58d97dad8f6f3611ba4d68
Tulsi Prasad <tulsi.prasad50@gmail.com>
2020-06-01 21:13:47 +0530

    :lipstick: update navbar links

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,43 +1,45 @@
 import React from "react";
+import FastfoodIcon from "@material-ui/icons/Fastfood";
+import { Link } from "react-router-dom";
+
 import "../../tailwind.generated.css";
 
 function NavBar() {
     return (
         <nav className="px-12 flex items-center justify-between flex-wrap bg-gray-800 p-6">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
-                <svg
-                    className="fill-current h-8 w-8 mr-2"
-                    width="54"
-                    height="54"
-                    viewBox="0 0 54 54"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-                </svg>
-                <span className="font-semibold text-xl tracking-tight">
-                    Foodies
-                </span>
+                <Link to="/">
+                    <span
+                        className="inline-block relative"
+                        style={{ top: "-4px" }}
+                    >
+                        <FastfoodIcon />
+                    </span>
+                    <span className="font-semibold ml-2 text-xl tracking-tight">
+                        Foodies
+                    </span>
+                </Link>
             </div>
             <div className="sm:block flex-grow flex items-center w-auto">
                 <div className="text-md flex-grow text-right">
-                    <a
-                        href="#responsive-header"
+                    <Link
+                        to="/"
                         className="block inline-block xl:mt-0 text-teal-200 hover:text-white mr-4"
                     >
                         Eat
-                    </a>
-                    <a
-                        href="#responsive-header"
+                    </Link>
+                    <Link
+                        to="/"
                         className="block inline-block xl:mt-0 text-teal-200 hover:text-white mr-4"
                     >
                         Sleep
-                    </a>
-                    <a
-                        href="#responsive-header"
+                    </Link>
+                    <Link
+                        to="/"
                         className="block inline-block xl:mt-0 text-teal-200 hover:text-white mr-4"
                     >
                         Repeat
-                    </a>
+                    </Link>
                 </div>
             </div>
         </nav>
